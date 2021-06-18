f2685f26852f05efec5baaaecbd6de216fe33471
Marcos Souza <mrcsz.m@gmail.com>
2021-06-18 11:07:53 -0300

    conversao de tempo

---
+++ iniciante/1019.py
@@ -0,0 +1,17 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1019
+
+
+seconds = int(input())
+
+hours = 0
+minutes = 0
+
+while seconds >= 60:
+    minutes += 1
+    seconds -= 60
+
+    if minutes >= 60:
+        hours += 1
+        minutes -= 60
+
+print(f"{hours}:{minutes}:{seconds}")
