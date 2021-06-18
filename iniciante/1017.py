e25351a0770a2d9d9ae8df4e351191cec4e932f2
Marcos Souza <mrcsz.m@gmail.com>
2021-06-18 00:33:51 -0300

    gasto de combustivel

---
+++ iniciante/1017.py
@@ -0,0 +1,7 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1017
+
+AUTONOMIA = 12
+tempo_gasto = int(input())
+velocidade_media = int(input())
+combustivel_necessario = (tempo_gasto * velocidade_media) / AUTONOMIA
+print(f"{combustivel_necessario:.3f}")
