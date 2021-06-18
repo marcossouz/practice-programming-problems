8271e4f4c846bc19fb3764fc60acce34c665746a
Marcos Souza <mrcsz.m@gmail.com>
2021-06-18 00:25:23 -0300

    fix descriptions

--- iniciante/1000.py
+++ iniciante/1000.py
@@ -1,11 +1,3 @@
-# Bem-vindo ao URI Online Judge!
-
-# O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
-
-# Entrada
-# Este problema não possui nenhuma entrada.
-
-# Saída
-# Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1000
 
 print('Hello World!')
--- iniciante/1013.py
+++ iniciante/1013.py
@@ -1,22 +1,4 @@
-# O Maior
-
-# Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
-
-# MaiorAB = (a + b + abs(a - b)) / 2
-# Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
-
-# Entrada
-# O arquivo de entrada contém três valores inteiros.
-
-# Saída
-# Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
-
-# Exemplos de Entrada	Exemplos de Saída
-# 7 14 106
-# 106 eh o maior
-
-# 217 14 6
-# 217 eh o maior
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1013
 
 a, b, c = input().split()
 a, b, c = int(a), int(b), int(c)
