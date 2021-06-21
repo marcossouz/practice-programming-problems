ffc6cfedd097fecf4ab94aba6f85597e837c8327
Marcos Souza <mrcsz.m@gmail.com>
2021-06-21 15:10:44 -0300

    fibonacci

---
+++ projecteuler.net/2.py
@@ -0,0 +1,33 @@
+# https://projecteuler.net/problem=2
+
+MAX_LIMIT = 4000000
+
+
+def fibonacci(n):
+    if n < 1:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def sum_even_terms():
+    sum_even = 0
+    i = 0
+    while True:
+        i += 1
+        result = fibonacci(i)
+        if result > MAX_LIMIT:
+            break
+
+        if result % 2 == 0:
+            sum_even += result
+
+    print(sum_even)
+
+
+if __name__ == '__main__':
+    sum_even_terms()
