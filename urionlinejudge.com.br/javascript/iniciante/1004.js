b90d57abbdfa6f4446fb5b661b826260c99eda0e
Marcos Souza <mrcsz.m@gmail.com>
2021-07-05 18:27:20 -0300

    1004 - prod
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

---
+++ urionlinejudge.com.br/javascript/iniciante/1004.js
@@ -0,0 +1,31 @@
+var readline = require('readline');
+var fs = require('fs');
+const { exit } = require('process');
+
+for(var i in [...Array(3).keys()]) {
+  var myInterface = readline.createInterface({
+    input: fs.createReadStream(`../inputData/1004/${i}.in`)
+  });
+
+  var lines = [];
+  myInterface.on('line', function (line) {
+    lines.push(line);
+  });
+  
+  var ii = 0
+  myInterface.on('close', function() {
+      // URI JUDGE INICIO
+
+      let a = parseInt(lines.shift());
+      let b = parseInt(lines.shift());
+      let result = `PROD = ${a * b}`;
+      
+      console.log(result);
+      // URI JUDGE FIM
+      ii++
+  });
+
+  myInterface.on('error', function(err) {
+      console.log(err)
+  });
+}
