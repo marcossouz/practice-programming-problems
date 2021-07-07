e29d52ef217f975935a940b9a1109ae417cf44bf
Marcos Souza <mrcsz.m@gmail.com>
2021-07-06 22:50:03 -0300

    106 - média 2
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

---
+++ urionlinejudge.com.br/javascript/iniciante/1006.js
@@ -0,0 +1,33 @@
+var readline = require('readline');
+var fs = require('fs');
+const { exit } = require('process');
+
+for(var i in [...Array(3).keys()]) {
+  var myInterface = readline.createInterface({
+    input: fs.createReadStream(`../inputData/1006/${i}.in`)
+  });
+
+  var lines = [];
+  myInterface.on('line', function (line) {
+    lines.push(line);
+  });
+  
+  var ii = 0
+  myInterface.on('close', function() {
+      // URI JUDGE INICIO
+
+      let a = parseFloat(lines.shift());
+      let b = parseFloat(lines.shift());
+      let c = parseFloat(lines.shift());
+      let media = ((a * 2) + (b * 3) + (c * 5)) / 10
+      let result = `MEDIA = ${media.toFixed(1)}`;
+      
+      console.log(result);
+      // URI JUDGE FIM
+      ii++
+  });
+
+  myInterface.on('error', function(err) {
+      console.log(err)
+  });
+}
