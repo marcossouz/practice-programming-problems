0064ed9f6cd16baa150fa3ef0d3266b9a52df72b
Marcos Souza <mrcsz.m@gmail.com>
2021-07-07 09:44:27 -0300

    1007 - diferença
    Signed-off-by: Marcos Souza <mrcsz.m@gmail.com>

---
+++ urionlinejudge.com.br/javascript/iniciante/1007.js
@@ -0,0 +1,34 @@
+var readline = require('readline');
+var fs = require('fs');
+const { exit } = require('process');
+
+for(var i in [...Array(3).keys()]) {
+  var myInterface = readline.createInterface({
+    input: fs.createReadStream(`../inputData/1007/${i}.in`)
+  });
+
+  var lines = [];
+  myInterface.on('line', function (line) {
+    lines.push(line);
+  });
+  
+  var ii = 0
+  myInterface.on('close', function() {
+      // URI JUDGE INICIO
+
+      let a = parseInt(lines.shift());
+      let b = parseInt(lines.shift());
+      let c = parseInt(lines.shift());
+      let d = parseInt(lines.shift());
+      let media = ((a * b) - (c * d))
+      let result = `DIFERENCA = ${media}`;
+      
+      console.log(result);
+      // URI JUDGE FIM
+      ii++
+  });
+
+  myInterface.on('error', function(err) {
+      console.log(err)
+  });
+}
